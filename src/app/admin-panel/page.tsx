90195d10831abf593b2cf92c8a7a60336227127c
Danieljeje <danieljeje330@gmail.com>
2026-07-30 23:40:37 +0100

    Fix AppLayout prop mismatch

--- src/app/admin-panel/page.tsx
+++ src/app/admin-panel/page.tsx
@@ -4,8 +4,8 @@ import AdminPanelContent from './components/AdminPanelContent';
 
 export default function AdminPanelPage() {
   return (
-    <AppLayout userRole="admin" userName="Pastor Chukwuemeka Obi" userDepartment="Senior Pastor">
-      <AdminPanelContent />
-    </AppLayout>
+    <AppLayout>
+  <AdminPanelContent />
+</AppLayout>
   );
 }
